4edb561dd1f0bd353bcd31851da45d7abd0ca077
zj565061763 <565061763@qq.com>
2023-06-06 14:13:27 +0800

    update

--- app/src/main/java/com/sd/demo/ctx/MainActivity.kt
+++ app/src/main/java/com/sd/demo/ctx/MainActivity.kt
@@ -2,14 +2,48 @@ package com.sd.demo.ctx
 
 import android.os.Bundle
 import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
-import com.sd.lib.ctx.fContext
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.sd.demo.ctx.ui.theme.AppTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        logMsg { "Activity onCreate context:$fContext" }
+        setContent {
+            AppTheme {
+                Content(
+                    onClick = {
+
+                    },
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun Content(
+    onClick: () -> Unit,
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(5.dp)
+    ) {
+        Button(
+            onClick = onClick
+        ) {
+            Text(text = "button")
+        }
     }
 }
 
