c815400a151b49378e1e88be278ab829a120078f
zjun <565061763@qq.com>
2023-12-21 19:47:49 +0800

    demo

--- app/src/main/java/com/sd/demo/ctx/MainActivity.kt
+++ app/src/main/java/com/sd/demo/ctx/MainActivity.kt
@@ -3,11 +3,33 @@ package com.sd.demo.ctx
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.sd.lib.ctx.FLastActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        logMsg { "onCreate last activity ${FLastActivity.get()}" }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        logMsg { "onStart last activity ${FLastActivity.get()}" }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        logMsg { "onResume last activity ${FLastActivity.get()}" }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        logMsg { "onPause last activity ${FLastActivity.get()}" }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        logMsg { "onStop last activity ${FLastActivity.get()}" }
     }
 }
 
