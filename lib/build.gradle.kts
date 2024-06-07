cc0463a26e1fd31bdea08dd6a2bb17fdcf355b87
zjun <565061763@qq.com>
2024-06-07 09:35:04 +0800

    1.3.5

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "ctx"
-val libVersionName = "1.3.4"
+val libVersionName = "1.3.5"
 
 android {
     namespace = "com.sd.lib.ctx"
